cfa180fb7aea7b16ede91d2fae20a72ccd2b6884
Elizaveta <74782333+LizaPuteshestvenniza@users.noreply.github.com>
2022-01-20 22:23:58 +0300

    Update difference_bigger_than_k.py

--- yandex_training1.0/hw5/lecture_tasks/difference_bigger_than_k.py
+++ yandex_training1.0/hw5/lecture_tasks/difference_bigger_than_k.py
@@ -8,3 +8,17 @@ for i in range(len(nums)):
             cnt += 1
 
 print(cnt)
+
+# Решение за O(n):
+nums = [int(i) for i in input().split()]
+k = int(input())
+cnt = 0
+j = 0
+for i in range(len(nums)):
+    while j < len(nums) and nums[j] - nums[i] <= k:
+        j += 1
+    if nums[j] - nums[i] > k:
+        cnt += len(nums) - j
+    if j == len(nums) - 1:
+        break
+print(cnt)
